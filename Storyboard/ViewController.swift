4f064c8af7368940ce83600333ce5e2ba21d49a0
huangluyang <huangluyang007@163.com>
2015-03-30 17:10:45 +0800

    Initial Commit

---
+++ Storyboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Storyboard
+//
+//  Created by huangluyang on 15/3/30.
+//  Copyright (c) 2015年 HLY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
